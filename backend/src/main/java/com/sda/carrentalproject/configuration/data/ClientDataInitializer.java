8fb515c0586c3274bc64ad79599c45b78af89c49
ETSirak <114828503+ETSirak@users.noreply.github.com>
2023-07-01 14:04:23 +0300

    ClientDataInitializer created only in develop profile

--- backend/src/main/java/com/sda/carrentalproject/configuration/data/ClientDataInitializer.java
+++ backend/src/main/java/com/sda/carrentalproject/configuration/data/ClientDataInitializer.java
@@ -4,6 +4,7 @@ import com.sda.carrentalproject.domain.Client;
 import com.sda.carrentalproject.repository.ClientRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -12,6 +13,7 @@ import java.time.Month;
 //TODO: move to develop profile
 @Component
 @Slf4j
+@Profile("develop")
 public class ClientDataInitializer implements CommandLineRunner {
 
     private final ClientRepository clientRepository;
