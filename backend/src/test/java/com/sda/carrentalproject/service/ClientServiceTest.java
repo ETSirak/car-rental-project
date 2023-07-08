e409de5c257144fcf5b0d47ce5c662151a4af00d
ETSirak <114828503+ETSirak@users.noreply.github.com>
2023-07-08 13:25:43 +0300

    creating new client test for ClientServiceTest

--- backend/src/test/java/com/sda/carrentalproject/service/ClientServiceTest.java
+++ backend/src/test/java/com/sda/carrentalproject/service/ClientServiceTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,6 +23,31 @@ class ClientServiceTest {
     @Test
     void checkInjectedService(){
         Assertions.assertNotNull(clientService, "Bean should be injected");
+    }
+
+    @Test
+    void creatingNewClient(){
+        //create new client
+        Client client = Client.builder()
+                .name("Marko")
+                .surname("Kalamees")
+                .phone("555-555-212")
+                .email("mk@et.et")
+                .address("Estonia")
+                .hasDrivingLicense(true)
+                .dateOfBirth(LocalDate.of(1991, Month.APRIL, 27))
+                .build();
+        //save it
+        var saved = clientService.saveClient(client);
+        //verify it after saving
+        Assertions.assertNotNull(saved.getId(), "Entity should have id!");
+        Assertions.assertEquals(client.getName(), saved.getName());
+        Assertions.assertEquals(client.getSurname(), saved.getSurname());
+        Assertions.assertEquals(client.getPhone(), saved.getPhone());
+        Assertions.assertEquals(client.getEmail(), saved.getEmail());
+        Assertions.assertEquals(client.getAddress(), saved.getAddress());
+        Assertions.assertEquals(client.getDateOfBirth(), saved.getDateOfBirth());
+        Assertions.assertEquals(client.getRegistrationDateTime(), saved.getRegistrationDateTime());
     }
 
     @BeforeEach
