e6aa884e3dd6e2f7423b0616c9b8325ac4552fbd
ETSirak <114828503+ETSirak@users.noreply.github.com>
2023-07-22 11:16:37 +0300

    beans validation added on create new client endpoint

--- backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
+++ backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
@@ -1,5 +1,9 @@
 package com.sda.carrentalproject.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -8,11 +12,19 @@ import java.time.LocalDateTime;
 @Builder
 public record ClientDto(
         Long id,
+        @NotNull
+                @Size(min = 2)
         String name,
+
+        @NotNull
+                @Size(min = 2)
         String surname,
 
+         @NotNull
+                 @Size(min = 7)
          String phone,
 
+         @Email
          String email,
 
          String address,
@@ -21,5 +33,6 @@ public record ClientDto(
 
          LocalDateTime registrationDateTime,
 
+         @Past
          LocalDate dateOfBirth) {
 }
